3fcfdeede9b8e992e6d55b458f0aee0cb688742c
abdoessordo <62575309+abdoessordo@users.noreply.github.com>
2020-04-21 15:26:47 +0000

    Add files via upload

--- congruence.js
+++ congruence.js
@@ -2,7 +2,18 @@ function RaiseToPower(form) {
     var a = form.a.value;
     var b = form.b.value;   
     var n = form.n.value;
+    console.log(a,b,n);
     
     var result = (a**b)%n
-    document.getElementById("demo").innerHTML = result;
+
+
+    if(isNaN(a) == false && isNaN(b) == false && isNaN(n) == false && isNaN(result) == false){
+        if (a**b > 10**308){
+            document.getElementById("demo").innerHTML = "Error, number is to big to calculate. Try a smaller one !"
+        }else{
+            document.getElementById("demo").innerHTML = a +'^' + b + ' ≡ ' + result + ' [' + n + ']';
+        }
+    }else{
+        document.getElementById("demo").innerHTML = "Please insert a correct input !";
+    }
 }
